be8cfb17d9e8928ada5dedb34b18774fb3c88773
Amit <maniclg1234@gmail.com>
2026-05-30 21:07:00 +0530

    upload program for day 3

---
+++ Day 3/rangeofprimeno.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Program2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        for (int n = start; n <= end; n++) {
+            boolean prime = true;
+
+            if (n <= 1)
+                prime = false;
+            else {
+                for (int i = 2; i < n; i++) {
+                    if (n % i == 0) {
+                        prime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (prime)
+                System.out.print(n + " ");
+        }
+    }
+}
