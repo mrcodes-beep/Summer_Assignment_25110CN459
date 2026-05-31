2a937d64cbdea3c9fab377a6463e5c58362f8ddd
Amit <maniclg1234@gmail.com>
2026-05-31 17:17:45 +0530

    upload all

--- Day 4/armstrongrange.java
+++ Day 4/armstrongrange.java
@@ -1,4 +1,30 @@
+import java.util.Scanner;
 
 public class armstrongrange {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end: ");
+        int end = sc.nextInt();
+
+        System.out.println("Armstrong Numbers:");
+
+        for (int num = start; num <= end; num++) {
+            int temp = num;
+            int sum = 0;
+
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += digit * digit * digit;
+                temp /= 10;
+            }
+
+            if (sum == num) {
+                System.out.print(num + " ");
+            }
+        }
+    }
 }
