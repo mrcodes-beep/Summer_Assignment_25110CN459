ff781a4c18f6067ff0e076ee0529fefa4fdbb6e6
Amit <maniclg1234@gmail.com>
2026-06-08 19:54:00 +0530

    upload day 12

---
+++ Day 12/perfectnofunc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class perfectnofunc {
+    static boolean isPerfect(int num) {
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPerfect(num)) {
+            System.out.println("Perfect Number");
+        } else {
+            System.out.println("Not a Perfect Number");
+        }
+    }
+}
