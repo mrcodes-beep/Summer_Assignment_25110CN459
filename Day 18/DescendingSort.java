eb41bada7da4f4a2514c17d48b3b7d661a2fe32b
Amit <maniclg1234@gmail.com>
2026-06-14 20:42:07 +0530

    ulpoad

---
+++ Day 18/DescendingSort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class DescendingSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Descending Sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Array in Descending Order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
