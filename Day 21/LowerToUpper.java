674b13cdb9d34fde6007593c034f99ff829349fb
Amit <maniclg1234@gmail.com>
2026-06-17 20:14:52 +0530

    day 21 upload

---
+++ Day 21/LowerToUpper.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LowerToUpper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a lowercase string: ");
+        String str = sc.nextLine();
+
+        String upper = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char)(ch - 32);
+            }
+
+            upper += ch;
+        }
+
+        System.out.println("Uppercase string = " + upper);
+    }
+}
